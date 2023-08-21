3b83ce3c6d5521635e6849e1d1d328160fe93c70
tomochinn <122351367+tomochinn@users.noreply.github.com>
2023-08-21 04:58:05 +0000

    完成

--- app/controllers/diarys_controller.rb
+++ app/controllers/diarys_controller.rb
@@ -1,23 +1,51 @@
 class DiarysController < ApplicationController
+  
   def index
+    @diarys = Diary.all
   end
 
   def new
+    @diary = Diary.new
   end
   
   def create
+    @diary = Diary.new(diary_params)
+    
+    if @diary.save
+      redirect_to diary_path(@diary.id)
+    else
+      render :new
+    end
+  end
+  
+  def show
+    @diary = Diary.find(params[:id])
   end
   
   def edit
+    @diary = Diary.find(params[:id])
   end
   
   def update
+    @diary = Diary.find(params[:id])
+    
+    if @diary.update(diary_params)
+      redirect_to diary_path(@diary.id)
+    else
+      render :edit
+    end
   end
 
-  def show
-  end
   
   def destroy
+    @diary = Diary.find(params[:id])
+    @diary.destroy
+    redirect_to diarys_path
+  end
+  
+  private
+  def diary_params
+    params.require(:diary).permit(:body)
   end
   
 end
